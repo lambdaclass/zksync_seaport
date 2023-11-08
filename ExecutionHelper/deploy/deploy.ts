aa24157c31f07c9583ae29f32d3acccfa18cb58b
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2023-11-08 17:28:25 -0300

    Save ExecutionHelper deployment address

--- ExecutionHelper/deploy/deploy.ts
+++ ExecutionHelper/deploy/deploy.ts
@@ -1,9 +1,17 @@
 import { deployContract } from "./utils";
+import * as fs from 'fs';
+
+function storeContractAddress(address: string) {
+  fs.writeFileSync(".executionHelper.address", address);
+}
+
 
 // An example of a basic deploy script
 // It will deploy a Greeter contract to selected network
 // as well as verify it on Block Explorer if possible for the network
-export default async function () {
+export default async function() {
   const contractArtifactName = "ExecutionHelper";
-  await deployContract(contractArtifactName);
+  const contract = await deployContract(contractArtifactName);
+
+  storeContractAddress(contract.address);
 }
