6b0f50f762f539542b8fa348027f98e248f1e109
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2023-11-08 17:29:02 -0300

    Remove outdated comment

--- ExecutionHelper/deploy/deploy.ts
+++ ExecutionHelper/deploy/deploy.ts
@@ -5,10 +5,6 @@ function storeContractAddress(address: string) {
   fs.writeFileSync(".executionHelper.address", address);
 }
 
-
-// An example of a basic deploy script
-// It will deploy a Greeter contract to selected network
-// as well as verify it on Block Explorer if possible for the network
 export default async function() {
   const contractArtifactName = "ExecutionHelper";
   const contract = await deployContract(contractArtifactName);
