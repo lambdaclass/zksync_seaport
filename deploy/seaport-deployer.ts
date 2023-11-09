c748585a43daf68afeded85520ea80c5d6b4165c
dafifynn <slimbieber@gmail.com>
2023-11-09 17:59:45 -0300

    follow seaport-deployer

--- deploy/seaport-deployer.ts
+++ deploy/seaport-deployer.ts
@@ -1,12 +1,11 @@
-import { deployContract } from "./utils";
+import { deployContract, getProvider } from "./utils";
 import { ethers } from 'ethers';
-import { HardhatRuntimeEnvironment } from "hardhat/types";
 
 const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
 
 if (!PRIVATE_KEY) throw "⛔️ Private key not detected! Add it to the .env file!";
 
-export default async function (hre: HardhatRuntimeEnvironment) {
+export default async function () {
 
   // Deploy CREATE2-related contracts
   const immutableCreate2FactoryInterfaceArtifact = "ImmutableCreate2FactoryInterface";
@@ -23,25 +22,20 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   const seaport = await deployContract(seaportArtifact, [coduitController.address]);
 
   // CREATE2 salt (20-byte caller or zero address + 12-byte salt).
-  const callerAddress = '0x0000000000000000000000000000000000000000'; // Replace with the actual caller address
-  const salt = '0x0000000000000000000000000000000000000000d4b6fcc21169b803f25d2210'; // 12-byte salt
+ const salt = '0x0000000000000000000000000000000000000000d4b6fcc21169b803f25d2210'; 
 
-  const combinedSalt = ethers.utils.hexZeroPad(callerAddress, 32) + salt.slice(2);
+  const combinedSalt = seaport.address + salt;
 
   const saltBytes32 = ethers.utils.arrayify(combinedSalt);
+  
+  const provider = await getProvider();
+  const transaction = await provider.getTransaction(seaport.deployTransaction.hash);
+  const creationCode = transaction.data;
 
-  // const ImmutableCreate2FactoryArtifact = await hre.artifacts.readArtifact("ImmutableCreate2FactoryInterface");
+  const seaport_address = await create2Interface.safeCreate2(saltBytes32, creationCode); 
+  console.log('Result:', creationCode);
 
-  // const aaFactory = new ethers.Contract(
-  //   AA_FACTORY_ADDRESS,
-  //   factoryArtifact.abi,
-  //   wallet,
-  // );
-  // // Packed and ABI-encoded contract bytecode and constructor arguments.
-  // bytes memory initCode = abi.encodePacked(
-  //   type(Seaport).creationCode,
-  //   abi.encode(CONDUIT_CONTROLLER)
-  // );
-
-  await create2Interface.safeCreate2(saltBytes32, )
+  if (seaport_address != seaport.address) {
+    throw new Error("address is not the spected");
+  }
 }
