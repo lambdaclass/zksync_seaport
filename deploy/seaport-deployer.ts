46a6906c3dd4ee962b468b220866ea950b4b362d
dafifynn <slimbieber@gmail.com>
2023-11-14 17:27:54 -0300

    add immutablecreate2 transferhelper deployment

--- deploy/seaport-deployer.ts
+++ deploy/seaport-deployer.ts
@@ -1,4 +1,5 @@
 import { deployContract, getProvider } from "./utils";
+import { ethers } from 'ethers';
 
 const PRIVATE_KEY = process.env.WALLET_PRIVATE_KEY || "";
 
@@ -8,7 +9,7 @@ export default async function () {
 
   // Deploy CREATE2-related contracts
   const immutableCreate2FactoryInterfaceArtifact = "ImmutableCreate2Factory";
-  const create2Interface = await deployContract(immutableCreate2FactoryInterfaceArtifact);
+  const immutableCreate2 = await deployContract(immutableCreate2FactoryInterfaceArtifact);
 
   // Deploy conduit-related contracts
   const coduitControllerArtifact = "ConduitController";
@@ -20,4 +21,24 @@ export default async function () {
   const seaportArtifact = "Seaport";
   const seaport = await deployContract(seaportArtifact, [coduitController.address]);
 
+  // Deploy Helpers contract
+  const transferHelperArtifact = "TransferHelper";
+  const transferHelper = await deployContract(transferHelperArtifact, [coduitController.address]);
+
+  const salt = "0x00000000000000000000000000000000000000000000fcc21169b803f25d2210";
+  const bytesFormat = ethers.utils.arrayify(salt);
+
+  
+  console.log('provider');
+  const provider = await getProvider();
+  console.log('transaction');
+  const transaction = await provider.getTransaction(transferHelper.deployTransaction.hash);
+  console.log('code');
+  const creationCode = transaction.data;
+  console.log('safecreate');
+
+  const transferHelper_address = await immutableCreate2.safeCreate2(bytesFormat , creationCode); 
+  console.log('Result:', transferHelper_address);
+
+ 
 }
